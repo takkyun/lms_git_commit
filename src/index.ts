9c3a123a916bfa0141de056a1add9fd8dc61d919
Takuya Otani <takuya.otani@serenebach.net>
2024-11-12 10:50:05 +1300

    Update import statements and add constructCommitMessage function for generating commit messages based on the staged diff. Added commit message format documentation in comments.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { LMStudioClient } from "@lmstudio/sdk";
+import { LLMSpecificModel, LMStudioClient } from "@lmstudio/sdk";
 import { exec } from "child_process";
 
 const client = new LMStudioClient();
@@ -14,6 +14,7 @@ async function main() {
     console.error('No staged changes found.');
     process.exit(1);
   }
+  constructCommitMessage(model, staged.diff);
 }
 main();
 
@@ -92,3 +93,68 @@ const getStagedDiff = async (excludeFiles?: string[]) => {
   };
 };
 
+const constructCommitMessage = async (model: LLMSpecificModel, diff: string) => {
+  const prediction = model.respond([
+    { role: "system", content: generatePrompt('English', 200, '') },
+    { role: "user", content: diff },
+  ], {
+    maxPredictedTokens: 100,
+    temperature: 0.7,
+  });
+  for await (const text of prediction) {
+    process.stdout.write(text);
+  }
+}
+
+const commitTypesArray = ['', 'conventional'] as const;
+type CommitType = (typeof commitTypesArray)[number];
+const commitTypeFormats: Record<CommitType, string> = {
+  '': '<commit message>',
+  conventional: '<type>(<optional scope>): <commit message>',
+};
+const specifyCommitFormat = (type: CommitType) =>
+  `The output response must be in format:\n${commitTypeFormats[type]}`;
+
+const commitTypes: Record<CommitType, string> = {
+  '': '',
+  /**
+   * References:
+   * Commitlint:
+   * https://github.com/conventional-changelog/commitlint/blob/18fbed7ea86ac0ec9d5449b4979b762ec4305a92/%40commitlint/config-conventional/index.js#L40-L100
+   *
+   * Conventional Changelog:
+   * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
+   */
+  conventional: `Choose a type from the type-to-description JSON below that best describes the git diff:\n${JSON.stringify(
+    {
+      docs: 'Documentation only changes',
+      style:
+        'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+      refactor: 'A code change that neither fixes a bug nor adds a feature',
+      perf: 'A code change that improves performance',
+      test: 'Adding missing tests or correcting existing tests',
+      build: 'Changes that affect the build system or external dependencies',
+      ci: 'Changes to our CI configuration files and scripts',
+      chore: "Other changes that don't modify src or test files",
+      revert: 'Reverts a previous commit',
+      feat: 'A new feature',
+      fix: 'A bug fix',
+    },
+    null,
+    2
+  )}`,
+};
+
+const generatePrompt = (
+  locale = 'en',
+  maxLength = 50,
+  type = '' as CommitType
+) =>
+  [
+    'Generate a concise git commit message written in present tense for the following code diff with the given specifications below:',
+    `Message language: ${locale}`,
+    `Commit message must be a maximum of ${maxLength} characters.`,
+    'Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.',
+    commitTypes[type],
+    specifyCommitFormat(type),
+  ].filter(Boolean).join('\n');
