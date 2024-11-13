527247ef463e15c66ad1e5e6ba90695426eb8efd
Takuya Otani <takuya.otani@serenebach.net>
2024-11-13 20:38:42 +1300

    refactor(src, prompt): Refactoring code to use imported functions and improve readability. Removed inline comments and added necessary imports from './prompt'. Also moved constants to individual files for clarity. Improved formatting throughout the file.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { LLMSpecificModel, LMStudioClient } from "@lmstudio/sdk";
 import { confirm } from "@clack/prompts";
 import { exec } from "child_process";
 import { assertGitRepo, getStagedDiff } from "./git";
+import { generatePrompt } from "./prompt";
 
 const client = new LMStudioClient();
 const defaultModel = 'QuantFactory/Mistral-Nemo-Japanese-Instruct-2408-GGUF/Mistral-Nemo-Japanese-Instruct-2408.Q4_0.gguf';
@@ -44,11 +45,6 @@ async function checkModels() {
   return await client.llm.get({ identifier: defaultModelIdentifier });
 }
 
-/**
- * The following code is from https://github.com/Nutlope/aicommits with some modifications.
- * Copyright (c) Hassan El Mghari
- */
-
 const constructCommitMessage = async (model: LLMSpecificModel, diff: string) => {
   const prediction = model.respond([
     { role: "system", content: generatePrompt('English', 200, 'conventional') },
@@ -64,55 +60,3 @@ const constructCommitMessage = async (model: LLMSpecificModel, diff: string) => 
   return message;
 }
 
-const commitTypesArray = ['', 'conventional'] as const;
-type CommitType = (typeof commitTypesArray)[number];
-const commitTypeFormats: Record<CommitType, string> = {
-  '': '<commit message>',
-  conventional: '<type>(<optional scope>): <commit message>',
-};
-const specifyCommitFormat = (type: CommitType) =>
-  `The output response must be in format:\n${commitTypeFormats[type]}`;
-
-const commitTypes: Record<CommitType, string> = {
-  '': '',
-  /**
-   * References:
-   * Commitlint:
-   * https://github.com/conventional-changelog/commitlint/blob/18fbed7ea86ac0ec9d5449b4979b762ec4305a92/%40commitlint/config-conventional/index.js#L40-L100
-   *
-   * Conventional Changelog:
-   * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
-   */
-  conventional: `Choose a type from the type-to-description JSON below that best describes the git diff:\n${JSON.stringify(
-    {
-      docs: 'Documentation only changes',
-      style:
-        'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-      refactor: 'A code change that neither fixes a bug nor adds a feature',
-      perf: 'A code change that improves performance',
-      test: 'Adding missing tests or correcting existing tests',
-      build: 'Changes that affect the build system or external dependencies',
-      ci: 'Changes to our CI configuration files and scripts',
-      chore: "Other changes that don't modify src or test files",
-      revert: 'Reverts a previous commit',
-      feat: 'A new feature',
-      fix: 'A bug fix',
-    },
-    null,
-    2
-  )}`,
-};
-
-const generatePrompt = (
-  locale = 'en',
-  maxLength = 50,
-  type = '' as CommitType
-) =>
-  [
-    'Generate a concise git commit message written in present tense for the following code diff with the given specifications below:',
-    `Message language: ${locale}`,
-    `Commit message must be a maximum of ${maxLength} characters.`,
-    'Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.',
-    commitTypes[type],
-    specifyCommitFormat(type),
-  ].filter(Boolean).join('\n');
