381ddb9ee681bbba02a17a3c8e64165ec867fb33
Takuya Otani <takuya.otani@serenebach.net>
2024-11-24 16:40:54 +1300

    refactor(index.ts): reorganize model loading logic in main

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import { assertGitRepo, getStagedDiff } from "./git";
 import { generatePrompt, isCommitType } from "./prompt";
 import { copyToClipboard } from "./copy";
 
-const client = new LMStudioClient();
 const defaultModel = 'lightblue/suzume-llama-3-8B-japanese-gguf/ggml-model-Q4_K_M.gguf';
 const defaultModelIdentifier = 'suzume-llama-3-8b-japanese';
 
 const checkModels = async () => {
+  const client =new LMStudioClient();
   const loadedLLMs = await client.llm.listLoaded();
   if (loadedLLMs.length === 0) {
     await client.llm.load(defaultModel, {
@@ -41,12 +41,6 @@ const getArgParam = (key: string): string | undefined => {
 }
 
 const main = async () => {
-  const model = await checkModels();
-  if (!model) {
-    console.error('Model not found.');
-    process.exit(1);
-  }
-  console.log('Model:', model.identifier);
   await assertGitRepo();
   const staged = await getStagedDiff();
   if (!staged || !staged.diff) {
@@ -58,8 +52,8 @@ const main = async () => {
   const len = parseInt(getArgParam('len') ?? '200');
   const type = getArgParam('type') ?? '';
   const clipboard = getArgParam('clipboard') === 'true';
-  const dryrun = getArgParam('dryrun') === 'true';
   const prompt = generatePrompt(locale, len, isCommitType(type) ? type : '');
+  const dryrun = getArgParam('dryrun') === 'true';
   if (dryrun) {
     const input = `**System:**\n${prompt}\n\n**User:**\n${staged.diff}`;
     console.log('Dry run:', input);
@@ -68,6 +62,12 @@ const main = async () => {
     }
     return;
   }
+  const model = await checkModels();
+  if (!model) {
+    console.error('Model not found.');
+    process.exit(1);
+  }
+  console.log('Model:', model.identifier);
   const response = await constructCommitMessage(model, staged.diff, prompt);
   const message = [prefix, response].filter(p => !!p).join(' ');
   const confirmed = await confirm({ message: `Use this commit message?\n---\n${message}\n` });
