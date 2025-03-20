6de887fc288fe1d1570be6aa1e0b8a906ec958b0
Takuya Otani <takuya.otani@serenebach.net>
2025-03-20 19:06:46 +1300

    refactor(src): Normalize import statements and handle WSL2 on Linux.

--- src/copy.ts
+++ src/copy.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import os from 'node:os';
 
 export const copyToClipboard = (text: string) => {
   const command = `echo "${text.replace(/\\/g, '\\\\').replace(/\`/g, '\\`').replace(/"/g, '\\"').replace(/\n/g, '\\n')}"`;
@@ -7,6 +8,10 @@ export const copyToClipboard = (text: string) => {
   } else if (process.platform === 'win32') {
     exec(`${command} | clip`);
   } else if (process.platform === 'linux') {
+    if (os.release().includes('microsoft-standard-WSL2')) {
+      exec(`${command} | clip.exe`);
+      return;
+    }
     exec(`${command} | xclip -selection clipboard -l`);
   } else {
     console.error('Unsupported platform:', process.platform);
